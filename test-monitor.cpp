a0f9971bf46f10d14a57a3b65788abdb4b532221
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 17:21:25 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,6 +1,6 @@
-#include "./monitor.h"  
 
 #include <random>
+#include "./monitor.h"
 #include <gtest/gtest.h>
 
 std::random_device rd;
@@ -30,6 +30,7 @@ TEST(Monitor, InvalidTemperatureRange) {
   EXPECT_FALSE(istempok(random_low_temp));
   EXPECT_FALSE(istempok(random_high_temp));
 }
+
 
 
 
