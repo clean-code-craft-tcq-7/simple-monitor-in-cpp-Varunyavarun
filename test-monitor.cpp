28640df58b53e738e700a74c0095c76b41770f11
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 15:14:49 +0530

    Update test-monitor.cpp
    update test case for valid and invalid temperature range check

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,18 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
+#include <stdlib.h> 
 
 TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
   ASSERT_FALSE(vitalsOk(99, 102, 70));
   ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+}
+
+TEST(Monitor, ValidTemperatureRange) {
+for(i=95 ; i>102 ; i++){
+  EXPECT_TRUE(istempok(i));
+}
+
+TEST(Monitor, InvalidTemperatureRange) {
+  EXPECT_FALSE(istempok(random_low_temp));
+  EXPECT_FALSE(istempok(random_high_temp));
 }
