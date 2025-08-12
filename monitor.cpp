dbfeeed248efa4012d3d4e48f78d2a1cf0e221fb
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 06:32:23 +0530

    Update monitor.cpp
    Reduce complexity

--- monitor.cpp
+++ monitor.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-void print() {
+void print(std :: string message) {
   for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep_for(seconds(1));
@@ -14,21 +14,35 @@ void print() {
     }
 }
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
+int istempok(float temperature) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
-    print();
+    print("Temperature is out of range");
     return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    print();
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    print();
+  } 
+  return 1;
+}
+
+int ispulseok(float pulseRate) {
+  if (pulseRate < 60 || pulseRate > 100) {
+    cout << "Pulse Rate is critical!\n";
+    print("Pulse Rate is out of range");
     return 0;
   }
   return 1;
+}
+
+int isspo2ok(float spo2) {
+  if (spo2 < 90) {
+    cout << "Oxygen Saturation is critical!\n";
+    print("Oxygen Saturation is out of range");
+    return 0;
+  }
+  return 1;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  return istempok(temperature) && ispulseok(pulseRate) && isspo2ok(spo2);  
 }
 
 
