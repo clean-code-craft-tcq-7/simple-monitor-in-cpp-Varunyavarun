ff83667313e513376023522d96189a2407878401
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 15:13:19 +0530

    Update monitor.h
    Generate random numbers for invalid temperature check

--- monitor.h
+++ monitor.h
@@ -1,3 +1,13 @@
 #pragma once
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
+
+
+srand(time(NULL)); 
+float low_temp_min = 1.0;
+float low_temp_max = 95.0;
+float random_low_temp = low_temp_min + ((float)rand() / RAND_MAX) * (low_temp_max - low_temp_min);
+
+float high_temp_min = 102.0;
+float high_temp_max = 200.0;
+float random_high_temp = high_temp_min + ((float)rand() / RAND_MAX) * (high_temp_max - high_temp_min);
