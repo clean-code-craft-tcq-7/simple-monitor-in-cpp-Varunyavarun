05c26cd568270073664f5265abe57e2934083d37
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 06:11:02 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-void print(){
+void print() {
   for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep_for(seconds(1));
