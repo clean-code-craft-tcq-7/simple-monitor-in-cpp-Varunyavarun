86bafde13bcc49be2ca8109d74c4a89ad32cc3ff
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 16:01:50 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -11,9 +11,10 @@ float low_temp_min = 1.0;
 float low_temp_max = 95.0;
 float random_low_temp = low_temp_min + ((float)rand() / RAND_MAX) * (low_temp_max - low_temp_min);
 
-float high_temp_min = 102.0;
+float high_temp_min = 103.0;
 float high_temp_max = 200.0;
 float random_high_temp = high_temp_min + ((float)rand() / RAND_MAX) * (high_temp_max - high_temp_min);
+
 
 
 
