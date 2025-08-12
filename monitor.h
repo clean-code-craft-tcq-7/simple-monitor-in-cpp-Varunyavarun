b49d03be95ad7a8c29d5f47d35b1644a9a8c06e8
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-12 15:20:31 +0530

    Update monitor.h
    Solving build errors

--- monitor.h
+++ monitor.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <stdlib.h>
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
 
@@ -11,3 +12,4 @@ float random_low_temp = low_temp_min + ((float)rand() / RAND_MAX) * (low_temp_ma
 float high_temp_min = 102.0;
 float high_temp_max = 200.0;
 float random_high_temp = high_temp_min + ((float)rand() / RAND_MAX) * (high_temp_max - high_temp_min);
+
