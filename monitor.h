fbfc83a1b012955fe3eb50dd5a98809b58142db7
Varunyavarun <varunyavarun2000@gmail.com>
2025-08-19 11:43:20 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -3,10 +3,15 @@
 #include <string>
 
 void Alert(const std::string& message);
+int isinrange(float value, float min, float max);
 int istempok(float temperature);
 int ispulseok(float pulseRate);
 int isspo2ok(float spo2);
-int vitalsOk(float temperature, float pulseRate, float spo2);
+int isbloodsugarok(float bloodsugar);
+int isbloodpressureok(float bloodpressure);
+int isrespirationok(float respiration);
+int vitalsOk(float temperature, float pulseRate, float spo2, float respiration, float bloodpressure, float bloodsugar);
+
 
 
 
